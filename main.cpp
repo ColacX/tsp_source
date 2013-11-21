129e3238b229ccc5e97b1e716aed57ce75eaf4a9
ColacX <DanielZheng1989@gmail.com>
2013-11-21 01:42:37 +0100

    updated: graphics

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <vector>
 #include <stdexcept>
+#include <sstream>
 
 #ifdef WIN32
 #include <Windows.h>
@@ -20,8 +21,6 @@
 #endif
 
 #define round(x) int((x) + 0.5f)
-
-SDL_Color rgba_color = {0xff, 0x00, 0x00, 0x00};
 
 struct Node
 {
@@ -46,7 +45,7 @@ int pathLength(const std::vector<Node>& nodes)
 	int length = 0;
 	for (size_t ii = 0; ii < nodes.size() - 1; ii++)
 	{
-		length += distance(nodes[ii], nodes[ii+1]);
+		length += distance(nodes[ii], nodes[ii + 1]);
 	}
 	length += distance(nodes.front(), nodes.back());
 	return length;
@@ -70,8 +69,7 @@ std::vector<int> allPermutations(std::vector<Node>& nodes)
 			}
 		}
 		permutation++;
-	}
-	while(std::next_permutation(nodes.begin(), nodes.end(), less_compare));
+	} while (std::next_permutation(nodes.begin(), nodes.end(), less_compare));
 
 	return std::move(path);
 }
@@ -98,142 +96,22 @@ std::vector<int> greedy(const std::vector<Node>& nodes)
 	return std::move(path);
 }
 
-struct SDLContext
-{
-	SDL_Window* window;
-	SDL_GLContext glContext;
-	TTF_Font* font;
-};
+std::vector<Node> nodes;
+std::vector<int> shortestPath;
 
-SDLContext initSDL()
-{
-	if (SDL_Init(SDL_INIT_EVENTS) != 0)
-		throw std::runtime_error("SDL_Init");
-
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-	SDL_Window* sdl_window;
-	sdl_window = SDL_CreateWindow("avalg13_project2_tsp", 10, 20, 1000, 1000, SDL_WINDOW_OPENGL);
-
-	SDL_GLContext sdl_gl_context = SDL_GL_CreateContext(sdl_window);
-	SDL_GL_MakeCurrent(sdl_window, sdl_gl_context);
-
-	if (TTF_Init() == -1)
-	{
-		__debugbreak();
-		throw std::runtime_error("TTF_Init");
-	}
-
-	TTF_Font* text_font = TTF_OpenFont("consola.ttf", 9);
-	if (!text_font)
-	{
-		__debugbreak();
-		throw std::runtime_error("TTF_OpenFont");
-	}
-
-	SDLContext context;
-	context.window = sdl_window;
-	context.glContext = sdl_gl_context;
-	context.font = text_font;
-	return context;
-}
-
-void mainLoop(SDLContext& context)
-{
-	bool program_running = true;
-	while (program_running)
-	{
-		//process sdl events
-		SDL_Event ev;
-		while (SDL_PollEvent(&ev))
-		{
-			switch (ev.type)
-			{
-			case SDL_QUIT:
-				program_running = false;
-				break;
-			case SDL_KEYDOWN:
-				switch (ev.key.keysym.sym)
-				{
-				case SDLK_ESCAPE:
-					exit(0);
-					break;
-				default:
-					break;
-				}
-				break;
-			case SDL_MOUSEBUTTONDOWN:
-				fprintf(stderr, "mouse down x:%d y:%d\n", ev.button.x, ev.button.y);
-				break;
-			default:
-				break;
-			}
-		}
-
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		//draw all nodes
-		{
-			SDL_Surface* sdl_surface = TTF_RenderText_Blended(
-				context.font, //TTF or OTF text font
-				"100",
-				rgba_color //text rgba_color
-				);
-
-			glEnable(GL_BLEND);
-			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-			glPixelZoom(1.0f, -1.0f);
-			glRasterPos2f(0, 0);
-			glDrawPixels(sdl_surface->w, sdl_surface->h, GL_RGBA, GL_UNSIGNED_BYTE, sdl_surface->pixels);
-			glDisable(GL_BLEND);
-			SDL_FreeSurface(sdl_surface);
-		}
-
-		//draw all edges
-		{
-			glEnable(GL_COLOR);
-			glBegin(GL_LINES);
-
-			float s = 0.1f; //scale
-			glColor4f(1, 0, 0, 1);
-			glVertex2f(0, 0);
-
-			glColor4f(0, 1, 0, 1);
-			glVertex2f(s, s);
-
-			glEnd();
-		}
-
-		SDL_GL_SwapWindow(context.window);
-	}
-}
-
-void destroySDLContext(SDLContext& context)
-{
-	SDL_GL_DeleteContext(context.glContext);
-	SDL_DestroyWindow(context.window);
-	TTF_CloseFont(context.font);
-	SDL_Quit();
-}
+#include "graphic.h"
 
 int main(int argc, char* argv[])
 {
 #ifdef WIN32
-	SDLContext context =  initSDL();
-	mainLoop(context);
-	destroySDLContext(context);
-
+	graphic::construct();
 	FILE* file = fopen("input0.txt", "r+");
 #else
 	FILE* file = stdin;
 #endif
 	size_t numNodes = 0;
 	fscanf(file, "%d", &numNodes);
-	std::vector<Node> nodes(numNodes);
+	nodes.resize(numNodes);
 
 	for (size_t ii = 0; ii < numNodes; ii++)
 	{
@@ -241,27 +119,27 @@ int main(int argc, char* argv[])
 		nodes[ii].index = ii;
 	}
 
-	std::vector<int> shortestPath;
-
-	//if(numNodes <= 11)
-	//{
+	if (numNodes <= 11)
+	{
 		shortestPath = allPermutations(nodes);
-	//}
-	//else
+	}
+	else
 	{
 		shortestPath = greedy(nodes);
 	}
 
-	for(int& index : shortestPath)
+	shortestPath = {7,8,9,3,2,5,6,0,1,4};
+
+	for (int& index : shortestPath)
 	{
 		std::cout << index << "\n";
 	}
 	std::cout << std::endl;
 
 #ifdef WIN32
+	graphic::draw_path();
+	graphic::run();
 	fprintf(stderr, "main: end\n");
-	getchar();
 #endif
 	return 0;
 }
-
