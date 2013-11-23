da81e79497c2dc6bd16e9bfff2ce40f4f9e4870d
Markus Westerlind <marwes91@gmail.com>
2013-11-23 13:26:47 +0100

    Rewrote opt2 to look for the best improvement each step

--- 2_opt.cpp
+++ 2_opt.cpp
@@ -1,41 +1,43 @@
 #include <vector>
+#include <algorithm>
 #include "Node.h"
 
 TSPResult opt2(std::vector<Node> nodes)
 {
 	while (true)
 	{
+		int bestStart = 0, bestEnd = 0;
+		int bestImprovement = 0;
 		for (int ii = 0; ii < int(nodes.size()) - 2; ii++)
 		{
 			for (int jj = ii + 1; jj < int(nodes.size()) - 1; jj++)
 			{
 				//ii is the start of the first sequence, jj the end of it
-				int oldConnection;
-				int newConnection;
-				if (ii == 0)
+				Node& beginA = nodes[ii];
+				Node& endA = nodes[jj];
+				Node& beginB = nodes[jj + 1];
+				Node& endB = ii == 0 ? nodes.back() : nodes[ii - 1];
+				int oldConnection = distance(endB, beginA) + distance(endA, beginB);
+				int newConnection = distance(endB, endA) + distance(beginA, beginB);
+				int improvement = oldConnection - newConnection;
+				if (improvement > bestImprovement)
 				{
-					oldConnection = distance(nodes.back(), nodes.front()) + distance(nodes[jj], nodes[jj + 1]);
-					newConnection = distance(nodes.back(), nodes[jj]) + distance(nodes.front(), nodes[jj + 1]);
-				}
-				else
-				{
-					oldConnection = distance(nodes[ii - 1], nodes[ii]) + distance(nodes[jj], nodes[jj + 1]);
-					newConnection = distance(nodes[ii - 1], nodes[jj]) + distance(nodes[ii], nodes[jj + 1]);
-				}
-				if (newConnection < oldConnection)
-				{
-					//Reverse the sequence since an improvement was found
-					int reversals = (jj - ii + 1) / 2;
-					for (int kk = 0; kk < reversals; kk++)
-					{
-						std::swap(nodes[ii + kk], nodes[jj - kk]);
-					}
-					continue;
+					bestStart = ii;
+					bestEnd = jj;
+					bestImprovement = improvement;
 				}
 			}
 		}
-		//No improvement was found
-		break;
+		if (bestImprovement > 0)
+		{
+			//Reverse the sequence since an improvement was found
+			std::reverse(nodes.begin() + bestStart, nodes.begin() + bestEnd + 1);
+		}
+		else
+		{
+			//No improvement was found
+			break;
+		}
 	}
 	TSPResult result;
 	result.path = getPathVector(nodes);
